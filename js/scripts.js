ce5a4bbf6eca30d8609a6c73f90f315d9bfddaaf
LAPTOP-57J009DG\piotr <kubus-o_2@tlen.pl>
2017-07-27 17:45:36 +0200

    Nadałem kolory w style.css, zaprogramowałem przyciski do przewijania w scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,32 @@
 // scripts.js
 $(function () {
-	var carouselList = $("#carousel ol");
-    var myVar = setInterval(Slide, 3000);
-    
-    function Slide() {
-        carouselList.animate({"marginLeft": -400}, 500, moveFirstSlide);
+	var carouselList = $("#carousel ul");
+    var myVar = setInterval(slideRight, 3000);
+    $left = $('#left');
+    $right = $('#right');
+   
+    $left.click(function() {
+        slideLeft();
+    });
+    $right.click(function() {
+        slideRight ();
+    });
+    function slideRight() {
+        carouselList.animate({"marginLeft": -800}, 500, moveFirstSlide);
+	}
+    function slideLeft() {
+        carouselList.animate({"marginLeft": 0}, 500, moveLastSlide);
 	}
     function moveFirstSlide() {
         var firstItem = carouselList.find("li:first");
         var lastItem = carouselList.find("li:last");
         lastItem.after(firstItem);
-        carouselList.css({marginLeft: 0});
+        carouselList.css({marginLeft: -400});
+    }
+    function moveLastSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        firstItem.before(lastItem);
+        carouselList.css({marginLeft: -400});
     }
 });
